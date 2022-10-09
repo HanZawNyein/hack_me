8c435667f51d9b531221a931059589ef3afbf7a5
HanZawNyein <hanzawnyineonline@gmail.com>
2022-10-09 07:25:31 +0630

    update CI-CD

--- src/main.py
+++ src/main.py
@@ -6,3 +6,8 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.get("/english-number/{number}")
+async def english_number(number: int):
+    return {"english-number": number}
